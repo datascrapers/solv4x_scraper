5dd837a1c6668ab6726a22a3da4544adbc3b0e12
Bijan Kazemi-Shirkadeh <kazemi-shirkadeh.bijan@student.greenriver.edu>
2022-05-22 13:57:39 -0700

    add Thomas' scraper
    changes:
     - code separated into functions
     - added some error checking and simple err() function
     - combine series into one API call
     - added a series to column name map
     - use df.merge() to get proper DataFrame output
     - added support for a YAML config file

--- main.py
+++
@@ -1,9 +0,0 @@
-import scraper
-
-def main():
-    eia_scraper = scraper.EIAScraper(output_type='pandas')
-
-    # just downloading to CWD for now.
-    eia_scraper.scrape().to_csv('EnergyData.csv')
-
-main()
--- scraper.py
+++ scraper.py
@@ -1,126 +1,164 @@
-import time
+import os, sys
+from pathlib import Path # used for config and CSV file creation
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, fields # used for config object
+import yaml # used for config file
 
 import requests
 import pandas as pd
 
-# NOTE: want dataclass(kw_only=True) for python 3.10+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
+# key: Series Id. This identifies the specific data we are targeting. You can find this in the EIA API documents.
+# val: Column names that will be used for this series ID.
+series_name_map = {
+  'EBA.CAL-ALL.DF.H':     'Demand Forecast',
+  'EBA.CAL-ALL.NG.SUN.H': 'Solar Generation',
+  'EBA.CAL-ALL.NG.WND.H': 'Wind Generation',
+}
+
+# log an error message then exit the program cleanly.
+def err(err_msg):
+  print(err_msg)
+  sys.exit(1)
+
+# returns EIA API output as json object
+def scrape():
+  URL = 'https://api.eia.gov/series/'
+  PARAMS = {
+    'api_key': config.EIA_APIKey,
+    'series_id': ';'.join(series_name_map),
+    'num': 24, # num is the number of rows to return for each series
+  }
+
+  try:
+    r = requests.get(URL, PARAMS)
+
+    r.raise_for_status()
+  except Exception as e:
+    err(f'GET request failed: {e}')
+
+  try:
+    json_data = r.json()
+  except Exception as e:
+    err(f'could not convert to JSON: {e}')
+
+  if json_data.get('data') and 'error' in json_data['data']:
+    err(f'API call error: {json_data["data"]["error"]}')
+
+  return json_data
+
+def json_to_pd(json_data):
+  df = None
+  # loop through series list returned by EIAs API and add them to a pandas dataframe
+  for series in json_data.get('series'):
+    series_name = series_name_map.get(series['series_id'])
+    if series_name is None:
+      err(f'No `{series["series_id"]}` key in series_name_map. Please add one manually.')
+
+    if df is None:
+      df = pd.DataFrame(series.get('data'),
+                        columns = ['Date', series_name])
+      df.set_index('Date', drop=True, inplace=True)
+    else:
+      df_to_merge = pd.DataFrame(series['data'],
+                                 columns = ['Date', series_name])
+
+      df = df.merge(df_to_merge, how='outer', left_on='Date', right_on='Date')
+
+  assert(df is not None)
+
+  return df
+
+def save_csv(df):
+  # create a csv from the DataFrame
+  df.to_csv(config.CSVFilePath)
+
+  # prints out dataframe
+  print(df)
+
+
+def upload_to_db(df):
+  # Initialize the database credentials
+  cred = credentials.Certificate(config.firestoreKeyPath)
+  firebase_admin.initialize_app(cred)
+
+  # Database Connection
+  db = firestore.client()
+
+  # Add Data From the Dataframe to the Database
+  for _, row in df.iterrows():
+    cols = {'Date': str(row['Date'])}
+    for col, val in row.items():
+      if col != 'Date':
+        cols[col] = int(val) # XXX does EIA ever return floats that have a fractional?
+
+    db.collection('California Renewable Energy') \
+      .document(row['Date'])                     \
+      .add(cols)
+
+# create a Config dataclass and initialize it with default config options
+# exceptions raised are caught in init_config()
 @dataclass
-class Scraper():
-  DEFAULT_OUTPUT_TYPE = 'json'
+class Config:
+  _config_dir: Path = Path.home() / '.datascrapers'
+  path: Path = None
 
-  url:         str  = None
-  params:      dict = None
-  output_type: str  = DEFAULT_OUTPUT_TYPE
+  EIA_APIKey: str = 'L08gD6TFlLdYhl1sJKagbCVA5AJmcjCVOlWbUEdz'
 
-  response: requests.Response = None
+  saveCSVFile: bool = True
+  CSVFilePath: Path = None
+
+  firestoreKeyPath: Path = None
+  saveToDatabase: bool = True
 
   def __post_init__(self):
-    if not self.url:
-      raise Exception('no URL specified.')
+    self._config_dir.mkdir(parents=True, exist_ok=True)
 
-    self._output = self._set_output_type(self.output_type)
+    self.path = self._config_dir / 'config.yml'
 
-  def _set_output_type(self, output_type):
-    output_func_map = {
-      'string': self.to_str,
-      'json':   self.to_json,
-      'pandas': self.to_pd,
-    }
+    self.CSVFilePath      = self._config_dir / 'EnergyData.csv'
+    self.firestoreKeyPath = self._config_dir / 'firestoreCreds.json'
 
-    output_func = output_func_map.get(output_type)
 
-    if not output_func:
-      raise Exception('output type not supported.')
+def init_config():
+  # `config` is global so that any function can access the config settings
+  global config
 
-    return output_func
-
-  def scrape(self):
+  try:
+    config = Config()
+  except Exception as e:
+    err(f'problem creating config object: {e}')
+    
+  # open the YAML config file and reassign options it specifies in Config
+  with open(config.path) as config_file:
     try:
-      self.response = requests.get(self.url, params=self.params)
+      config_file = yaml.safe_load(config_file) # loaded as a dict
 
-      return self._output()
+      for opt, val in config_file.items():
+        if hasattr(config, opt):
+          setattr(config, opt, val)
+        else:
+          print(f'ignoring invalid option `{opt} in config file.')
+    except yaml.YAMLError as e:
+      print(f'problem loading config file: {e}')
+      print('using default config settings.')
 
-    except Exception as e: # of course, we'd want to actually check specific exceptions
-      print(f'scraper.py: scrape(): problem sending HTTP request: {e}')
+def main():
+  init_config()
 
-      return None
+  json_output = scrape()
+  df = json_to_pd(json_output)
 
-  def to_str(self) -> str:
-    if self.response is None:
-      raise Exception('No response')
+  if config.saveCSVFile:
+    save_csv(df)
 
-    return self.response.text
+  if config.saveToDatabase:
+    upload_to_db(df)
 
-  def to_json(self) -> dict:
-    if self.response is None:
-      raise Exception('No response')
+  return 0
 
-    try:
-      json_obj = self.response.json()
-
-      self.response.raise_for_status()
-
-      return json_obj
-    except Exception as e:
-      print(f'scraper.py: to_json(): could not convert to a JSON object: {e})')
-
-      return None
-
-  def to_pd(self):
-    pass
-
-@dataclass
-class EIAScraper(Scraper):
-
-  series_ids: list = field(default_factory=lambda: [
-    'EBA.US48-ALL.DF.H',
-    'EBA.CAL-ALL.NG.SUN.H',
-    'EBA.CAL-ALL.NG.WND.H',
-  ])
-
-  url:    str  = 'https://api.eia.gov/series/'
-  params: dict = field(default_factory=lambda: {
-      'api_key':   'g0m4Rggs1pnUJyMrHxlbzelyaS9GVcyeoIqqoo28',
-      'series_id': '',
-      'start':     time.strftime("%Y%m%d"), # start == current day
-  })
-
-  def __post_init__(self):
-    super().__post_init__()
-
-    self.params['series_id'] = self._construct_series_id()
-
-  def _construct_series_id(self):
-    return ';'.join(self.series_ids)
-
-  # override to_json to get the exact data we need
-  def to_json(self):
-    json_obj = super().to_json()
-
-    try:
-      # filter the series dicts to only contain name and data keys
-      return [ { k: series[k] for k in {'name', 'data'} } for series in json_obj['series'] ]
-    except (KeyError, IndexError):
-      raise Exception(json_obj['data'].get('error') or json_obj)
-
-  def to_pd(self):
-    json_obj = self.to_json()
-
-    df = None
-    for series in json_obj:
-      if df is None:
-        df = pd.DataFrame(series['data'],
-                          columns = ['Date', series['name']])
-
-        df.set_index('Date', drop=True, inplace=True)
-      else:
-        tmp_df = pd.DataFrame(series['data'],
-                              columns = ['Date', series['name']])
-        # outer merge because we don't want to assume which column will
-        # be largest.
-        df = df.merge(tmp_df, how='outer', left_on='Date', right_on='Date')
-
-    return df.sort_values(by=['Date'], ascending=False)
+if __name__ == '__main__':
+  sys.exit(main())
